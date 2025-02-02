4a536da57298591aa6eb2ace196d319ed2a751e1
aneeza1234 <aneezakokab@gmail.com>
2025-02-02 20:16:33 +0000

    datatypes

---
+++ .devcontainer/01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as newer version
+
+// alert( 3+3) // we are using nodejs, not browser
+
+console.log(3+3) //code readability should be high
+
+console.log("akchemist")
+
+let name = "akchemist"
+let age = 25
+let isLoggedIn = false
+
+// number => 2 to power 53
+//bigint => stock market, facebook
+//string => ""
+//boolean => true/false 
+//null => standalone value
+//undefined =>
+//symbol => find uniqueness
+
+
+//object
+
+
+console.log(typeof"akchemist")
+console.log(typeof age)
+console.log(typeof null) //object
+console.log(typeof undefined) //undefined
