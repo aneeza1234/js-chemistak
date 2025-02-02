c6376f59050f80b53ffe75b9f6b4c30ecda3ec76
aneeza1234 <aneezakokab@gmail.com>
2025-02-02 21:09:57 +0000

    conversion

---
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -0,0 +1,56 @@
+//let score = "33abc"
+
+//console.log(typeof score)
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber)
+//console.log(valueInNumber) 
+//NaN means not a number
+
+//let score = null
+//console.log(score)
+//let valueInNumber = Number(score)
+//console.log(valueInNumber) 
+
+//let score = undefined
+//console.log(score)
+//let valueInNumber = Number(score)
+//console.log(valueInNumber) 
+
+
+//let score = true
+//console.log(score)
+//let valueInNumber = Number(score)
+//console.log(valueInNumber) 
+
+
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//let isLoggedIn = "akchemist"
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//let isLoggedIn = ""
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+//let isLoggedIn = 0
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// 1 => true; 0=> false
+//"" => true
+//"akchemist" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber);
+
